1bb98b4f5e071ab666c268178740ef9e79a18cd0
Asuka Hirano(Win) <asukajo12345@gmail.com>
2025-11-20 22:16:28 +0900

    CSS周りの修正

--- src/main/java/com/example/demo/form/profile/ProfileEditForm.java
+++ src/main/java/com/example/demo/form/profile/ProfileEditForm.java
@@ -11,8 +11,11 @@ public class ProfileEditForm {
 	private String user_id;
 	@Size(max=32, message="32文字以内で指定してください。")
 	private String profile_name;
+	@Size(min = 4, max = 4, message = "数字4桁で入力してください")
     private String birth_year;
+	@Size(min = 2, max = 2, message = "数字4桁で入力してください。例）2月生まれの場合、「02」と入力。")
 	private String birth_month;
+	@Size(min = 2, max = 2, message = "数字4桁で入力してください。例）1日生まれの場合、「01」と入力。")
 	private String birth_day;
 	@Size(min=1, max=32, message="1文字から32文字で指定してください。")
 	private String saving_goal;
